911c68daa47362ed634d1b9e5b86d215ccf1dbb6
flannerysucks <142702780+flannerysucks@users.noreply.github.com>
2023-08-24 19:21:41 -0500

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,36 @@
-export default async function getPostText() {
-  // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Hello from the Bluesky API";
+import * as fs from 'fs';
+
+export default async function getPostText(): Promise<string | null> {
+  const today = new Date();
+  const monthDayString = today.toISOString().substring(5, 10); // Format: MM-DD
+
+  const filePath = '72seasonscalendar.txt';
+
+  try {
+    const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+    const lines = fileContent.split('\n');
+    
+    for (const line of lines) {
+      const lineDate = line.substring(0, 5); // Extract MM-DD from the line
+      if (lineDate === monthDayString) {
+        const textAfterDate = line.substring(6).trim(); // Extract the text after the date and remove leading/trailing whitespace
+        return textAfterDate;
+      }
+    }
+    
+    return null; // No post found for today's date.
+  } catch (error) {
+    throw new Error(`Error reading file: ${error}`);
+  }
 }
+
+// Example usage
+getPostText()
+  .then(text => {
+    if (text !== null) {
+      console.log(text);
+    } else {
+      console.log("No post found for today's date.");
+    }
+  })
+  .catch(error => console.error(error));
